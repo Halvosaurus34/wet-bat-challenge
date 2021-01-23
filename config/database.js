c17bb172ed738ece5f2f79f806f1d729bf68be59
Levi Halvorson <lhalvorson34@gmail.com>
2021-01-23 16:15:32 -0700

    moved console log inside db.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
+console.log("JAWSDB CONNECTION........", process.enc.JAWSDB_URL)
 
 if (process.env.JAWSDB_URL) {
-  console.log("JAWSDB CONNECTION........", process.enc.JAWSDB_URL)
   connection = new Sequelize(process.env.JAWSDB_URL)
 } else {
   connection = new Sequelize('wetbat_db', 'root', 'KpkhcBk3PL4n', {
