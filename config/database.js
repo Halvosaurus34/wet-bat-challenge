05270013a14999600407580c27048d0d11c71d74
Levi Halvorson <lhalvorson34@gmail.com>
2021-01-23 15:44:04 -0700

    changed connection to include jawsdb

--- config/database.js
+++ config/database.js
@@ -1,13 +1,20 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('wetbat_db', 'root', 'KpkhcBk3PL4n', {
-  host: 'localhost',
-  dialect: 'mysql',
-  
-  pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle:10000
-  },
-});
+
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  connection = new Sequelize('wetbat_db', 'root', 'KpkhcBk3PL4n', {
+    host: 'localhost',
+    dialect: 'mysql',
+    
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle:10000
+    },
+  });
+}
+
+module.exports = connection;
