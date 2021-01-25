21b241a07103745275b1588cca3280b1dc1c4da6
Levi Halvorson <lhalvorson34@gmail.com>
2021-01-24 18:20:44 -0700

    put quotes route above * route in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const path = require('path')
+const PORT = process.env.PORT || 5000
+const app = express();
 
 // Database
 const db = require('./config/database')
@@ -10,17 +12,15 @@ db.authenticate()
     .then(()=> console.log('Database connected...'))
     .catch(err=> console.log(err))
 
-const app = express();
 app.use(express.static("client/build"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/quotesTable', require('./routes/quotes'))
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, `client/build/index.html`));
 });
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+
 //Quotes routes
-app.use('/quotesTable', require('./routes/quotes'))
-console.log(path.join(__dirname, '/client/build/index.html'))
-const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
